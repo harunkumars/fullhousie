adbcc7b8b86165453aeebbbaba3ba67c16e16451
harunkumars <harun@betacraft.io>
2023-04-10 11:50:06 +0530

    create admin user in prod

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,4 +15,4 @@ end
 
 game.start
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') #if Rails.env.development?
