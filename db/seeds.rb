bb70dcd89125ed36bf5eac75f9d988591b6a1092
harunkumars <harun@betacraft.io>
2024-02-20 17:08:38 +0530

    make db seeds interactive

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-game = Game.create!(name: Time.now)
+STDOUT.puts "Enter Game Name (e.g. Meetup Feb 2024): "
+game_name = STDIN.gets.chomp
+game = Game.create!(name: game_name.strip.presence || Time.now)
 
-20.times do |i|
-  player = Player.create!(name: "Player_#{i+1}")
+STDOUT.puts "Enter number of players (default 5): "
+num_players = STDIN.gets.chomp.to_i
+num_players = num_players.positive? ? num_players : 5
+
+num_players.times do |i|
+  STDOUT.puts "Enter Player Name (default: Player_#{i+1}): "
+  player_name = STDIN.gets.chomp
+
+  player = Player.create!(name: player_name.strip.presence || "Player_#{i+1}")
   game.game_sessions.create!(player: player)
 end
 
 game.start
+STDOUT.puts "Game #{game.name} started with #{game.players.count} players."
+STDOUT.puts "Share the following card link to each player:"
+game.game_sessions.each do |game_session|
+  STDOUT.puts "#{game_session.player.name}: /cards/#{game_session.card.to_param}"
+end
+STDOUT.puts "Game #{game.name} is ready to play!"
+STDOUT.puts "Game master can visit the following link to view the game:"
+STDOUT.puts "/games/#{game.to_param}"
 
+STDOUT.puts "Seed complete!"
