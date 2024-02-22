685bacca6fed6fd3556be5acc2c053b84b2a84e1
harunkumars <harun@betacraft.io>
2024-02-22 22:27:14 +0530

    cleanup controller, routes, seed file

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,4 +3,8 @@ class AdminUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[email]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'lotteries/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cards, only: [:show]
-  resources :games, only: [:show, :update]
   resources :lotteries, only: [:update]
+  root 'admin/dashboard#index'
 end
